3429dbb67c4fac75f1855a14b53bf5c86e9ef3d2
chrischne <chrischne@gmail.com>
2017-05-29 11:41:17 +0200

    enhance config.js with screepro configuration

--- public/config.js
+++ public/config.js
@@ -1,7 +1,16 @@
 function getHost(){
-	var htw = true;
+	var htw = false;
+	var screenpro = false;
+	var intothemind = true;
 
-	if(htw){
+	if(intothemind){
+		return '10.0.143.18:8080';
+	}
+	else if(screenpro){
+		//TODO put ip of thingelithing
+		return '127.0.0.1:8080';
+	}
+	else if(htw){
 		return '10.0.143.18:8080';
 	}
 	else return '127.0.0.1:8080';
