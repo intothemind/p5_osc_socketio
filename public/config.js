77d572aa024f2963d4fffdb1a68effce1afcb955
chrischne <chrischne@gmail.com>
2017-05-29 13:43:33 +0200

    added things to config

--- public/config.js
+++ public/config.js
@@ -4,10 +4,10 @@ function getHost(){
 	var intothemind = true;
 
 	if(intothemind){
-		return '10.0.143.18:8080';
+		return '10.0.1.2:8080';
 	}
 	else if(screenpro){
-		//TODO put ip of thingelithing
+		//TODO put ip of screenpro network
 		return '127.0.0.1:8080';
 	}
 	else if(htw){
