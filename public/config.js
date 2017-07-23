1b0d71b16308efd47c97c5a4ee8ba133145718a5
chrischne <chrischne@gmail.com>
2017-07-23 14:29:40 +0200

    config for local network

--- public/config.js
+++ public/config.js
@@ -2,6 +2,7 @@ function getHost(){
 	var htw = false;
 	var screenpro = false;
 	var intothemind = false;
+	var localNetwork = true;
 
 	if(intothemind){
 		return '10.0.1.4:8080';
@@ -12,6 +13,9 @@ function getHost(){
 	}
 	else if(htw){
 		return '10.0.143.18:8080';
+	}
+	else if(localNetwork){
+		return '169.254.30.216:8080';
 	}
 	else return '127.0.0.1:8080';
 }
