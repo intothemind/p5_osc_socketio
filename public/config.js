e7c8616ba7874868ca80af6b32c1b9646f53ffa6
chrischne <chrischne@gmail.com>
2019-03-08 21:32:50 +0100

    remove default network

--- public/config.js
+++ public/config.js
@@ -2,7 +2,7 @@ function getHost(){
 	var htw = false;
 	var screenpro = false;
 	var intothemind = false;
-	var localNetwork = true;
+	var localNetwork = false;
 
 	if(intothemind){
 		return '10.0.1.4:8080';
@@ -16,7 +16,7 @@ function getHost(){
 	}
 	else if(localNetwork){
 		//return '169.254.30.216:8080';
-		return '192.168.1.25:8080';
+		return '192.168.1.45:8080';
 	}
 	else return '127.0.0.1:8080';
 }
